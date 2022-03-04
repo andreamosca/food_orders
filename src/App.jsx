7c490672c588a971567082c57e3a5ff1be8bf383
andreamosca <andrea.mosca.prod@gmail.com>
2022-03-04 20:49:32 +0100

    added Cart Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react"
 import "./App.css"
+import Cart from "./components/Cart/Cart"
 import Header from "./components/Layout/Header/Header"
 import Meals from "./components/Meals/Meals"
 
 function App() {
   return (
     <Fragment>
+      <Cart></Cart>
       <Header/>
       <main>
         <Meals></Meals>
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,61 @@
+import Modal from "../UI/Modal"
+import classes from "./Cart.module.css"
+
+const cartItems = [
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  }
+]
+
+const Cart = props => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        {
+          id: "m1",
+          name: "Sushi",
+          description: "Finest fish and veggies",
+          price: 22.99,
+        },
+      ].map(item => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  )
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>€22.99</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  )
+}
+
+export default Cart
