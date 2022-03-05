b7398d6eef26ad2f4ba672e9b803b9c9ff6caffb
andreamosca <andrea.mosca.prod@gmail.com>
2022-03-05 01:38:13 +0100

    implementing CartProvider

--- src/components/Layout/Header/HeaderCartButton.jsx
+++ src/components/Layout/Header/HeaderCartButton.jsx
@@ -1,14 +1,23 @@
+import { useContext } from "react"
+import CartContext from "../../../store/cart-context"
 import CartIcon from "../../Cart/CartIcon"
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton = props => {
+  const cartCtx = useContext(CartContext)
+
+  const numberOfItems = cartCtx.items.reduce(
+    (total, item) => total + item.amount,
+    0
+  )
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Il tuo carrello</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   )
 }
