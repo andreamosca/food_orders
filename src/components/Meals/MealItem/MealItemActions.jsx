518d828f56ca148edf50cdb95c748925170a7396
VittorioS <vittorio.scognamiglio@hotmail.com>
2022-03-05 11:44:24 +0100

    Un porco non si smentisce mai!

--- src/components/Meals/MealItem/MealItemActions.jsx
+++ src/components/Meals/MealItem/MealItemActions.jsx
@@ -2,8 +2,12 @@ import classes from "./MealItemActions.module.css"
 import Input from "../../UI/Input"
 
 const MealItemActions = props => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <form className={classes.form} action="#">
+    <form className={classes.form} action="#" onSubmit={submitHandler}>
       <Input
         label="Amount"
         input={{
