e1c4cddf3e1dbcbe21e241b2de094c88801d5c1e
andreamosca <andrea.mosca.prod@gmail.com>
2022-03-05 16:35:52 +0100

    updated JSDoc

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -5,11 +5,17 @@ const CartContext = React.createContext({
   totalAmount: 0,
   /**
    * Receive the item it should be added
-   * @method
+   * @param {Object} item - The item to add
+   * @param {string} item.id - The id of the item
+   * @param {string} item.name - The name of the item
+   * @param {string} item.description - The description of the item
+   * @param {number} item.price - The price of the item
+   * @param {number} item.amount - The amount of the item to buy 
    */
   addItem: (item) => {},
   /**
    * Receive the item already existing in the cart which should be removed
+   * @param {string} id - The id of the item
    * @method
    */
   removeItem: (id) => {},
