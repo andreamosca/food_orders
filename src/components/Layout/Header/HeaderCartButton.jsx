2e6482102e5c5cc4a2c90780910679ec36c8113b
andreamosca <andrea.mosca.prod@gmail.com>
2022-03-10 16:25:09 +0100

    added animation on number of items change with useEffect

--- src/components/Layout/Header/HeaderCartButton.jsx
+++ src/components/Layout/Header/HeaderCartButton.jsx
@@ -1,18 +1,33 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import CartContext from "../../../store/cart-context"
 import CartIcon from "../../Cart/CartIcon"
 import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton = props => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false)
   const cartCtx = useContext(CartContext)
+  const {items} = cartCtx
 
-  const numberOfItems = cartCtx.items.reduce(
-    (total, item) => total + item.amount,
-    0
-  )
+  const numberOfItems = items.reduce((total, item) => total + item.amount, 0)
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`
+
+  useEffect(() => {
+    if(items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true)
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [numberOfItems])
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
